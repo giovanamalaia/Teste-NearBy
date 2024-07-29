3c24ee54f04b0e0bb6d658f6f856a9bde8270b2c
Apple <>
2024-07-29 23:20:40 +0000

    Resubmission.

--- NIPeekaboo/MultipeerConnectivitySupport/MPCSession.swift
+++ NIPeekaboo/MultipeerConnectivitySupport/MPCSession.swift
@@ -1,5 +1,5 @@
 /*
-See LICENSE folder for this sample’s licensing information.
+See the LICENSE.txt file for this sample’s licensing information.
 
 Abstract:
 A class that manages peer discovery-token exchange over the local network by using MultipeerConnectivity.
